c99a0a425e6d06b72b1c036cae29166ee346a211
cobra <alank.dev@gmail.com>
2018-08-16 08:41:20 +0200

    Add bundle.js

---
+++ bundle.js
@@ -0,0 +1,57 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+module.exports = Phrase;
+
+String.prototype.reverse = function(){return Array.from(this).reverse().join("");
+}
+function reverse(string) {
+	return Array.from(string).reverse().join("");
+}
+function Phrase(content) { 
+	this.content = content;
+
+	this.processor = function(string){
+		return string.toLowerCase();
+	}
+	this.processedContent = function () {
+		//return this.processor(this.content);
+		return this.letters().toLowerCase();
+
+	};
+	this.letters = function letters() {
+		let theLetters = [];
+		for (let i = 0; i < this.content.length; i++) {
+			if (this.content.charAt(i).match(/[a-zA-Z]/)) {
+				theLetters.push(this.content.charAt(i));
+			}
+		}
+		return theLetters.join("");
+	}
+	this.palindrome = function palindrome() {
+		return this.processedContent() === reverse(this.processedContent());
+	}
+}
+function TranslatedPhrase(content, translation){
+	this.content = content;
+	this.translation = translation;
+	this.processedContent = function () {
+		return this.processor(this.translation);
+	}
+}
+TranslatedPhrase.prototype = new Phrase();
+
+
+},{}],2:[function(require,module,exports){
+let Phrase = require("goidel-palindrome");
+let string = prompt("Please enter a string for palindrome testing:");
+
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+	alert(`"${phrase.content}" is a palindrome!`)
+} else {
+	alert(`"${phrase.content}"" is not a palindrome.`)
+}
+// after making changes to this file, remeber to run broserify again
+// browserify main.js -o bundle.js
+
+},{"goidel-palindrome":1}]},{},[2]);
